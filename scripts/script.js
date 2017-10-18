c75625a4f9397d2b1adbfcf5383719df07e12fbf
Seakibble <max.leeming@gmail.com>
2017-10-18 08:40:33 -0400

    Made the correct project a bit better.

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,39 @@
-
 function buildToc () {
     var h2s = $('h2');
-    var toc = $('#toc');
+    var list = [];
     h2s.each(function (i) {
-        console.log(i);
+        toc = $("#toc");
         heading = $(this);
         var id = heading.text().toLowerCase().replace(/ /g,'');
         heading.prepend((i+1) + ". ");
         heading.attr('id', id);
         toc.append('<a href="#'+id+'"><span class="toc_heading">'+heading.text()+'</span></a>');
+        list.push(heading.offset().top);
     });
+    
+    
+    return list;
+}
+
+
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function tocUpdate() {
+    if (window.pageYOffset >= sticky) {
+        toc[0].classList.add("sticky")
+    } else {
+        toc[0].classList.remove("sticky");
+    }
+
+    var foundActive = false;
+    if (offsetList != undefined) {
+        for (var i = 0; i < offsetList.length; i++) {
+            if (offsetList[i]-10 >= window.pageYOffset && !foundActive) {
+                $('#toc a:nth-child('+(i+1)+') span').addClass("active");
+                foundActive = true;
+            } else {
+                $('#toc a:nth-child('+(i+1)+') span').removeClass("active");
+            }
+        }
+    }
 }
